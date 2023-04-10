1051e0da14a7823fd1d61e06e634ef56068ba7cd
pocke <quinnhuynh1126@gmail.com>
2023-04-10 16:08:43 -0700

    revised about me page

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="pb-5">
+    <div class="container text-center pb-3 pb-md-4"></div>
+    <h1 class="h3 fw-normal py-3 py-md-4">About Me</h1> 
+    <div class="mb-3">hello my name is quyen</div>
   </div>
 </template>
